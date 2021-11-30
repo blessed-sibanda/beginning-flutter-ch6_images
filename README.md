0b001663db10c40ab563898c2dec08dd87e64e11
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 14:21:26 +0200

    rename project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# starter_app
+# ch6_images
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/starter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/starter_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.starter_app
+package com.example.ch6_images
 
 import io.flutter.embedding.android.FlutterActivity
 
