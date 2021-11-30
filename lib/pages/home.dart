1739628a2a8420c1cb22db9c2c44dbe789d34303
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 18:14:05 +0200

    images and icon

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,7 +14,45 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: const [
+                ImagesAndIconWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImagesAndIconWidget extends StatelessWidget {
+  const ImagesAndIconWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image(
+          image: const AssetImage('assets/images/logo.png'),
+          width: MediaQuery.of(context).size.width / 3,
+          fit: BoxFit.cover,
+        ),
+        Image.network(
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+          width: MediaQuery.of(context).size.width / 3,
+        ),
+        const Icon(
+          Icons.brush,
+          color: Colors.lightBlue,
+          size: 48.0,
+        ),
+      ],
     );
   }
 }
