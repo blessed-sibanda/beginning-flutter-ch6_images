f9ada9b40b0abcba38fcf26900851aadbf9124eb
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 18:30:41 +0200

    decorators

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,9 +21,69 @@ class _HomeState extends State<Home> {
             child: Column(
               children: const [
                 ImagesAndIconWidget(),
+                Divider(),
+                BoxDecoratorWidget(),
+                Divider(),
+                InputDecoratorsWidget(),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class InputDecoratorsWidget extends StatelessWidget {
+  const InputDecoratorsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      TextField(
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          color: Colors.grey.shade800,
+          fontSize: 16.0,
+        ),
+        decoration: const InputDecoration(
+          labelText: 'Notes',
+          labelStyle: TextStyle(color: Colors.purple),
+          // border: UnderlineInputBorder(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.purple),
+          ),
+          border: OutlineInputBorder(),
+        ),
+      ),
+      const Divider(height: 50.0),
+      TextFormField(
+        decoration: const InputDecoration(labelText: 'Enter your notes'),
+      ),
+    ]);
+  }
+}
+
+class BoxDecoratorWidget extends StatelessWidget {
+  const BoxDecoratorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: Colors.orange,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 10.0),
+            ),
+          ],
         ),
       ),
     );
